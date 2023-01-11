d62e90821d1c5f604f88f5b3e9bffd12768c2dfc
Ravens83 <verne83@hotmail.com>
2023-01-11 13:06:28 +0100

    First commit

---
+++ FountainOfObjectsUI/Program.cs
@@ -0,0 +1,9 @@
+﻿using FountainOfObjectsClassLib;
+
+FountainGame game = new FountainGame(Cavern.CavernSizes.small);
+
+game.RunGame();
+/*game._ui.PrintLocation(game._player);
+
+Console.WriteLine(game._cavern.TryToMove(game._player,2,2));
+game._ui.PrintLocation(game._player);*/
